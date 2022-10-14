337133882815ef4af14db299ebad02200dd9215c
Rodrigo Barrera <36526580+Cracop@users.noreply.github.com>
2022-10-14 10:01:54 -0500

    Revert "Resolviendo bugs (#2)" (#3)
    This reverts commit 5a408ff025279ac4fa36d860382bc969c1d351ad.

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,15 +1,8 @@
 export function validateRegisterForm(datos){
-    //let ans = [true,""];
     if (datos.nombre==="" || datos.apellido==="" || datos.correo==="" || datos.passwd === "") {
-        return [false,"Todos los campos deben contener información"];  
+        return false;
     }
-    if (datos.passwd.length < 6) {
-        return [false,"La contraseña debe ser al menos 6 caracteres"];  
-    }
-    if (!datos.correo.endsWith("@itam.mx")) {
-        return [false,"Se debe utilizar el correo del ITAM"];  
-    }
-    return [true,""]
+    return true
 }
 
 
