0dbef3f895217c892a35c147d77f58b62d669ed3
Rodrigo Barrera <36526580+Cracop@users.noreply.github.com>
2022-10-18 19:21:35 -0500

    Integrando backend (#7)
    * Se importó axios y se declararon las funciones en la store
    
    * ya es posible la integración con el back
    
    * Ya se integró con el backend la parte de agregar, buscar si existe y confirmar
    
    * cambios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+
 import "./assets/tailwind.css";
 import { createPinia } from 'pinia'
 
@@ -9,5 +10,6 @@ const app = createApp(App).use(createPinia());
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
+
 app.mount("#app");
 
