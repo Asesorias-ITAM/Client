3c4146b624df45e329677d0ff10c8301fbe7827d
fcalod <fcalodiz@itam.mx>
2022-10-27 11:59:34 -0500

    Empezar tarjetas de publicación

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,30 @@
 <template>
     <div class="grid grid-cols-7 bgfondo-dark-1">
-        <div class="col-span-1 min-h-[90.2vh] bg-verde-itam-2">
-            <div class="flex flex-col justify-items-center px-14 text-center text-2xl">
-                <div class=""></div>
-                <MenuButton text="Buscar" type="unPressed" class="mt-20"/>
-                <MenuButton text="Mis Asesores" type="unPressed" class="mt-10"/>
-                <MenuButton text="Mis Grupos" type="pressed" class="mt-10"/>
-                <div class="my-48"></div>
-                <MenuButton text="Mi Perfil" type="unPressed" class=""/>
-                <MenuButton text="Cerrar Sesión" type="unPressed" @click="logout" class="mt-10"/>
+        <div class="page_container">
+            <div class="col-span-1 min-h-[90.2vh] bg-verde-itam-2">
+                <div class="flex flex-col justify-items-center px-14 text-center text-2xl">
+                    <div class=""></div>
+                    <MenuButton text="Buscar" type="unPressed" class="mt-20"/>
+                    <MenuButton text="Mis Asesores" type="unPressed" class="mt-10"/>
+                    <MenuButton text="Mis Grupos" type="pressed" class="mt-10"/>
+                    <div class="my-48"></div>
+                    <MenuButton text="Mi Perfil" type="unPressed" class=""/>
+                    <MenuButton text="Cerrar Sesión" type="unPressed" @click="logout" class="mt-10"/>
+                </div>
+            </div>
+            <div class="col-span-6 min-h-[90.2vh]">
+                <router-view name="grid"></router-view>
+                <div class="card_container">
+                    <div class="card">
+                        <div class="content">
+                            <span>Jane Doe</span> 
+                            <span>Interior Designer</span> 
+                        </div>
+                    </div>
+                </div>
+                <!--{{store.email}}-->
             </div>
         </div>
-        <div class="col-span-6 min-h-[90.2vh]">
-            <router-view name="grid"></router-view>
-            {{store.email}}
-        </div>
-
     </div>
 
 </template>
@@ -64,5 +73,48 @@ export default {
 <style scoped>
 *{
     border: 0px solid red;
+}
+
+.page_container {
+    display: grid;
+    grid-template-columns: minmax(min-content, 175px) minmax(max-content, 1fr) minmax(
+        max-content,
+        400px
+    );
+    width: 90%;
+	max-width: 1280px;
+	margin-left: auto;
+	margin-right: auto;
+	column-gap: 4rem;
+	align-items: flex-end;
+    @media (max-width: 1200px) {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-bottom: 1px solid var(--c-gray-600);
+	}
+}
+
+.card {
+    @media (max-width: 1200px) {
+		display: none;
+	}
+  position:relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+}
+
+.card_container {
+    display: grid;
+    grid-gap: 35px;
+    margin: 0 auto;
+    padding: 0px 40px;
+    
 }
 </style>
