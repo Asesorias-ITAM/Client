fbc0e6239d01b0f945ae63dccb96aea8bc1ff659
fcalod <fcalodiz@itam.mx>
2022-11-08 21:32:07 -0600

    Corregir typo en SearchGroupsPage

--- src/views/SearchGroupsPage.vue
+++ src/views/SearchGroupsPage.vue
@@ -1,19 +1,20 @@
 <template>
-    <div> Esta es la página de búsqueda </div>
     <section class="card-grid my-3">
         <div class="grid grid-cols-4 gap-6">
-            <PublicationCard v-for="pub of pub_list" :key="pub.id" v-bind="pub" class="min-h-24 overflow-auto"/>
+            <PublicationCard v-for="pub of pub_list" :key="pub.id" v-bind="pub"/>
         </div>
     </section>
 </template>
 
 <script>
 import PublicationCard from "@/components/PublicationCard.vue"
+
+
 let placeholder_desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget lacus lectus. Nullam sed augue eget elit viverra dignissim. Ut nec urna in."
 
 export default {
     name: "SearchGroupsPage",
-    componentes: { PublicationCard },
+    components: { PublicationCard },
     data() {
         return {
             pub_list: [{id: 0, materia: "Economía I", asesor: "Juan Acosta", desc: placeholder_desc},
