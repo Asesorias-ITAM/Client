818db15d59ef24b8b967c61cfa2b0aebf56fed4f
Cracop <rodrigoalejandrobarrera@gmail.com>
2022-11-19 09:27:03 -0600

    Ya muestra la carta del alumno seleccionado

--- src/components/dashboard/TarjetaAlumno.vue
+++ src/components/dashboard/TarjetaAlumno.vue
@@ -1,15 +1,20 @@
 <template>
     <article class="col-span-1 min-w-8 min-h-48 rounded-lg overflow-hidden shadow-md bg-fondo-tarjeta-1 dark:bg-fondo-dark-tarjeta-1">
         <div class="bg-fondo-tarjeta-2 dark:bg-fondo-dark-tarjeta-2">
-            <h1 class="px-3 py-4 text-3xl font-bold">h</h1>
-            {{alumno}}
+            <h1 class="px-3 pt-4 pb-2 text-3xl font-bold">
+                {{alumno.nombre+ " "+alumno.apellido}}
+            </h1>
+            <h2 class="mx-3 mb-3 text-xl">{{alumno.correo}}</h2>
+            <h3 class="mx-3 text-xl" v-if="alumno.telefono!==null">{{alumno.telefono}}</h3>
         </div>
-        <h2 class="mx-3 my-5 text-xl">a</h2>
-        <p  class="mx-3 my-5 text-base">d</p>
+        <h3 class="mx-3 my-5 text-xl">{{"Asesor: "+ (alumno.asesor ? "Si" : "No")}}</h3>
+        <h3 class="mx-3 mt-5 text-xl" v-if="alumno.carrera.length>0">{{"Estudia: "}}</h3>
+        <h4 v-for="carrera in alumno.carrera" :key="carrera"> {{carrera}}</h4>
+        
+        
         <footer class="card-footer">
-            <button class="px-2 py-1 mx-2.5 rounded-md bg-fondo-tarjeta-2 dark:bg-fondo-dark-tarjeta-2">Ver curso</button>
-            <button class="px-2 py-1 rounded-md bg-fondo-tarjeta-2 dark:bg-fondo-dark-tarjeta-2">Inscribirme</button>
-        </footer>
+            
+        </footer>   
     </article>
 </template>
 
